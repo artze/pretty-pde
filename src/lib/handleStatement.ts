f07f6afbe26689052a3278fa63d1d17b0c75f508
artze <arthurwtm@gmail.com>
2021-08-10 18:24:12 +0800

    Remove console.log

--- src/lib/handleStatement.ts
+++ src/lib/handleStatement.ts
@@ -8,20 +8,14 @@ export function handleStatement(
   for (let i = 0; i < document.lineCount; i++) {
     const line = document.lineAt(i);
 
-    console.log("start", line.range.start);
-    console.log("end", line.range.end);
-
     // Handle assignment statement
     if (line.text.match(/\s*=\s*/)) {
-      console.log("assign");
-      console.log(line.text);
       const lhs = line.text.substring(0, line.text.indexOf("=")).trim();
       const rhs = line.text
         .substring(line.text.indexOf("=") + 1)
         .trim()
         .replace(/;/g, "");
       const r = `${lhs} = ${rhs};`;
-      console.log(r);
       edit.push(
         vscode.TextEdit.replace(
           line.range.with(
